6e9193ebde3a98a1198d0efd6e83e6fe425df953
ayush81z <ayushkumar811op@gmail.com>
2026-03-29 10:07:08 +0530

    sets and maps uses

---
+++ src/Hashmaps/implementation.java
@@ -0,0 +1,62 @@
+package Hashmaps;
+
+import java.util.*;
+
+public class implementation {
+
+    public static void main(String[] args) {
+//        String name =  "hola";
+//        int hashval = name.hashCode();//would be converted into a numeric format using some formulas
+//        System.out.println(hashval);
+//
+//        Integer a = 1; //the number stays the same in hashcode
+//        int b = a.hashCode();
+//        System.out.println(b);
+
+
+//        //hashmaps
+//        HashMap<String , Integer> map = new HashMap<>();
+//
+//        map.put("apple" , 56);
+//        map.put("banana", 57);
+//        map.put("cabbage", 90);
+//
+//        System.out.println(map.getOrDefault("apple", 78));
+//        System.out.println(map.get("banana"));
+//        System.out.println(map.containsKey("hiifen"));
+
+
+
+//        //hashset : it could filter outs the duplicate values , sorting is not always a thing (depends on hashcode()
+//        HashSet<Integer> set = new HashSet<>();
+//        set.add(90);
+//        set.add(91);
+//        set.add(89);
+//        set.add(23);
+//        set.add(90);
+//
+//        System.out.println(set);
+
+        //same as linkedhashmap
+        //LinkedHashset : the same order in which you inserted values
+//        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+//        set.add(90);
+//        set.add(88);
+//        set.add(22);
+//        set.add(77);
+//
+//        System.out.println(set);
+
+        //same as treemap
+        //treeset : A TreeSet is a collection class that stores unique elements in a sorted order.
+//        TreeSet<Integer> t = new TreeSet<>();
+//        t.add(30);
+//        t.add(22);
+//        t.add(34);
+//        t.add(21);
+//        t.add(90);
+//
+//        System.out.println(t);
+
+    }
+}
