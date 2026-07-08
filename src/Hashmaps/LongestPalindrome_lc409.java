30bad207732e9ae2e94de4b0bdba46c7d56141f2
ayush81z <ayushkumar811op@gmail.com>
2026-07-08 22:50:20 +0530

    using basic maps

---
+++ src/Hashmaps/LongestPalindrome_lc409.java
@@ -0,0 +1,39 @@
+package Hashmaps;
+
+import java.util.HashMap;
+
+public class LongestPalindrome_lc409 {
+    public static void main(String[] args) {
+        String s = "abccccdd";
+
+        LongestPalindrome_lc409 obj = new LongestPalindrome_lc409();
+        System.out.println(obj.longestPalindrome(s));
+    }
+
+    public int longestPalindrome(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int length = 0;
+        boolean oddFound = false;
+
+        for (int count : map.values()) {
+            if (count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                oddFound = true;
+            }
+        }
+
+        if (oddFound) {
+            length++;
+        }
+
+        return length;
+    }
+}
